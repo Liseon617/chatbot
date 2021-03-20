5b20a4607a9b467fbcf73f1bbf17d3d615482971
Liseon <blzl.l.gtsj@gmail.com>
2021-03-20 20:59:04 +0800

    port assigned and chatbot completed

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-  
 const path = require("path");
 const express = require("express");
 const colors = require("colors");
@@ -16,9 +15,55 @@ app.use(express.static(path.join(__dirname, "public")));
 const PORT = process.env.PORT || 3000;
 
 const server = app.listen(
-    PORT,
-    console.log(
-      `Server is runnig on ${process.env.NODE_ENV} mode at port ${PORT}`.yellow
-        .bold
-    )
-  );
+  PORT,
+  console.log(
+    `Server is runnig on ${process.env.NODE_ENV} mode at port ${PORT}`.yellow
+      .bold
+  )
+);
+
+const io = socketio(server);
+io.on("connection", function (socket) {
+  console.log("a user connected");
+
+  socket.on("chat message", (message) => {
+    console.log(message);
+
+    const callapibot = async (projectId = process.env.PROJECT_ID) => {
+      try {
+        const sessionId = uuid.v4();
+        const sessionClient = new dialogflow.SessionsClient({
+          keyFilename: "./chatbot-kxaf-866d1fe26402.json",
+        });
+        const sessionPath = sessionClient.projectAgentSessionPath(
+          projectId,
+          sessionId
+        );
+        const request = {
+          session: sessionPath,
+          queryInput: {
+            text: {
+              text: message,
+              languageCode: "en-US",
+            },
+          },
+        };
+        const responses = await sessionClient.detectIntent(request);
+
+        console.log("Detected intent");
+        const result = responses[0].queryResult.fulfillmentText;
+        socket.emit("bot reply", result);
+        console.log(result);
+        if (result.intent) {
+          console.log(`  Intent: ${result.intent.displayName}`);
+        } else {
+          console.log(`  No intent matched.`);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    callapibot();
+  });
+});
