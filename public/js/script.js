304c498e4f01411bdf88b2873c3795a4cea4a640
Liseon <blzl.l.gtsj@gmail.com>
2021-03-22 01:12:03 +0800

    integrated new interface
    New interface anchors chatbot to the bottom right side of the screen.

--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,12 @@
-const btn = document.querySelector("button");
+/*const btn = document.querySelector("button");
 const outputme = document.querySelector(".output-you");
-const outputbot = document.querySelector(".output-bot");
+const outputbot = document.querySelector(".output-bot");*/
+const popup = document.querySelector('.chat-popup');
+const chatBtn = document.querySelector('.chat-btn');
+const submitBtn = document.querySelector('.send');
+const micBtn = document.querySelector('.mic')
+const chatArea = document.querySelector('.chat-area');
+const inputElm = document.querySelector('input');
 const socket = io();
 
 const SpeechRecognition =
@@ -11,7 +17,7 @@ const recognition = new SpeechRecognition();
 recognition.lang = "en-US";
 recognition.interimResults = false;
 
-btn.addEventListener("click", () => {
+micBtn.addEventListener("click", () => {
   recognition.start();
 });
 
@@ -20,8 +26,12 @@ recognition.onresult = function (event) {
   const text = event.results[last][0].transcript;
   console.log(text);
 
-  outputme.textContent = text;
-
+  let temp = `<div class="outgoing-msg">
+  <span class="my-msg">${text}</span>
+  <img src="img/me.jpg" class="avatar">
+  </div>`;
+  chatArea.insertAdjacentHTML("beforeend", temp);
+  chatArea.scrollTop = chatArea.scrollHeight;
   socket.emit("chat message", text);
 };
 
@@ -31,10 +41,33 @@ const botReply = (text) => {
   utterance.text = text;
   utterance.pitch = 1;
   utterance.volume = 1;
+
   synth.speak(utterance);
 };
 
 socket.on("bot reply", (text) => {
-  outputbot.textContent = text;
-  botReply(text);
+    let temp = `<div class="incoming-msg">
+    <img src="img/bot.jpg" class="avatar">
+    <span class="bot-msg">${text}</span>
+    </div>`;
+    chatArea.insertAdjacentHTML("beforeend", temp);
+    chatArea.scrollTop = chatArea.scrollHeight;
+    botReply(text);
 });
+
+submitBtn.addEventListener('click', ()=> {
+    let userInput = inputElm.value;
+    let temp = `<div class="outgoing-msg">
+    <span class="my-msg">${userInput}</span>
+    <img src="img/me.jpg" class="avatar">
+    </div>`;
+
+    chatArea.insertAdjacentHTML("beforeend", temp);
+    chatArea.scrollTop = chatArea.scrollHeight;
+    inputElm.value = "";
+    socket.emit("chat message", userInput);
+})
+
+chatBtn.addEventListener('click', ()=>{
+    popup.classList.toggle('show');
+})
