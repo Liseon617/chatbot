f6a0e57907076af282d43e6778f009f75b47f66c
Liseon <blzl.l.gtsj@gmail.com>
2021-04-20 21:20:08 +0800

    minor changes

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const path = require("path");
 const express = require("express");
-const colors = require("colors");
 const dotenv = require("dotenv");
 const socketio = require("socket.io");
 const dialogflow = require("@google-cloud/dialogflow");
@@ -17,8 +16,7 @@ const PORT = process.env.PORT || 3000;
 const server = app.listen(
   PORT,
   console.log(
-    `Server is runnig on ${process.env.NODE_ENV} mode at port ${PORT}`.yellow
-      .bold
+    `Server is runnig on ${process.env.NODE_ENV} mode at port ${PORT}`
   )
 );
 
